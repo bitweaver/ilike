a55e8acbc80578d750008d7a6a02ae63294ada0e
Max Kremmel <xing@synapse.plus.com>
2007-04-01 18:36:25 +0000

    add menu entry

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -5,4 +5,13 @@ $registerHash = array(
 	'package_path' => dirname( __FILE__ ).'/',
 );
 $gBitSystem->registerPackage( $registerHash );
+
+if( $gBitSystem->isPackageActive( 'ilike' )) {
+	$menuHash = array(
+		'package_name'  => ILIKE_PKG_NAME,
+		'index_url'     => ILIKE_PKG_URL.'index.php',
+		'menu_template' => 'bitpackage:ilike/menu_ilike.tpl',
+	);
+	$gBitSystem->registerAppMenu( $menuHash );
+}
 ?>
