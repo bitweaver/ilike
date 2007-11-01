53218735f477df1296d33e005b336b505dae7fe1
bitweaver.org <bitweaver@users.sourceforge.net>
2007-11-01 07:06:42 +0000

    sort content types alphabetically

--- modules/mod_search.php
+++ modules/mod_search.php
@@ -1,10 +1,11 @@
 <?php
 global $gLibertySystem, $module_rows, $module_params, $module_title;
 if( empty( $contentTypes ) ) {
-	$contentTypes = array( '' => tra( 'All Content' ) );
 	foreach( $gLibertySystem->mContentTypes as $cType ) {
 		$contentTypes[$cType['content_type_guid']] = $cType['content_description'];
 	}
 }
+asort( $contentTypes );
+$contentTypes = array_merge( array( '' => tra( 'All Content' )), $contentTypes );
 $gBitSmarty->assign( 'contentTypes', $contentTypes );
 ?>
